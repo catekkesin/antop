f082ead66f1c189df7b66992089001ad7761c74b
catekkesin <cemalardatekkesin@gmail.com>
2023-06-30 13:21:48 +0300

    ram gb indicator implemented

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { MultiProgressBars } from "multi-progress-bars";
 import * as chalk from "chalk";
 import os from "os";
 import cpuStats from "cpu-stats";
+import { spawn } from "child_process";
 
 export async function run() {
   const readCPU = () => {
@@ -19,57 +20,82 @@ export async function run() {
     const freeMemory = os.freemem();
     const usedMemory = totalMemory - freeMemory;
 
-    return usedMemory / totalMemory;
+    return {
+      percentage: usedMemory / totalMemory,
+      totalMemory: (totalMemory / (1024 * 1024 * 1024)).toFixed(2),
+      usedMemory: (usedMemory / (1024 * 1024 * 1024)).toFixed(2),
+    };
   };
 
   // Initialize mpb
   const mpb = new MultiProgressBars({
-    initMessage: " antop :) ",
+    initMessage: ` antop :) ver. ${process.env.npm_package_version} `,
     anchor: "top",
     persist: true,
     border: true,
   });
 
-  // TASKS start
-
-  mpb.addTask("RAM", {
-    type: "percentage",
-    barTransformFn: chalk.green,
-    nameTransformFn: chalk.bold,
-  });
-
-  // TASKS end
-
+  // create tasks start
   const createCPULoadTasks = () => {
     const coreCount = os.cpus().length; // Get the number of CPU cores
 
     for (let i = 0; i < coreCount; i++) {
       mpb.addTask(`Core${i}`, {
         type: "percentage",
-        barTransformFn: chalk.green,
+        barTransformFn: chalk.red,
         nameTransformFn: chalk.bold,
       });
     }
   };
 
+  const createRAMLoadTask = () => {
+    mpb.addTask("RAM", {
+      type: "percentage",
+      barTransformFn: chalk.green,
+      nameTransformFn: chalk.bold,
+    });
+  };
+  // create task end
+
+  // update task start
   const updateCPULoadTasks = async () => {
     const cpuData = await readCPU();
 
     for (let i = 0; i < cpuData.length; i++) {
       console.log();
 
-      mpb.updateTask(`Core${i}`, { percentage: cpuData[i].cpu / 100 });
+      mpb.updateTask(`Core${i}`, {
+        percentage: cpuData[i].cpu / 100,
+      });
     }
   };
 
-  const checkCPUTemperature = () => {};
+  const updateRAMLoadTask = () => {
+    let memInfo = readMem();
+
+    mpb.updateTask("RAM", {
+      percentage: memInfo.percentage,
+      message: `${memInfo.usedMemory} / ${memInfo.totalMemory} G`,
+    });
+  };
+  // update task end
+
+  // for now is not working
+  // i will implement.
+  const checkCPUTemperature = () => {
+    let temp = spawn("cat", ["/sys/class/thermal/thermal_zone0/temp"]);
+
+    temp.stdout.on("data", function (data) {
+      console.log("Result: " + data / 1000 + " degrees Celcius");
+    });
+  };
 
   createCPULoadTasks();
+  createRAMLoadTask();
 
   setInterval(async () => {
     updateCPULoadTasks();
-
-    mpb.updateTask("RAM", { percentage: readMem() });
+    updateRAMLoadTask();
   }, 500);
 
   await mpb.promise;
